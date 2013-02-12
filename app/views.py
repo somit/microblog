6896eda2a26277faeefb7986dc46b37972ec4048
somit <somit.pisces@gmail.com>
2013-02-12 16:42:15 +0530

    views.py modifed

--- app/views.py
+++ app/views.py
@@ -1,6 +1,21 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = { 'nickname': 'SOmit' }
+    posts = [ # fake array of posts
+        { 
+            'author': { 'nickname': 'zoom' }, 
+            'body': 'Cool' 
+        },
+        { 
+            'author': { 'nickname': 'tim' }, 
+            'body': 'The code was so cool!' 
+       }
+             ]
+    return render_template("index.html",
+        title = 'HomePage',
+        user = user,
+        posts=posts)
